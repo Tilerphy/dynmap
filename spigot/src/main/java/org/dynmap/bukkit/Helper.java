7b02d2ba6043d9525417d8113c63d291396c12d2
tilerphy <346410840@163.com>
2019-12-12 23:41:12 +0800

    change something for 1.15

--- spigot/src/main/java/org/dynmap/bukkit/Helper.java
+++ spigot/src/main/java/org/dynmap/bukkit/Helper.java
@@ -9,13 +9,13 @@ import org.dynmap.bukkit.helper.v113.BukkitVersionHelperSpigot113;
 import org.dynmap.bukkit.helper.v113_1.BukkitVersionHelperSpigot113_1;
 import org.dynmap.bukkit.helper.v113_2.BukkitVersionHelperSpigot113_2;
 import org.dynmap.bukkit.helper.v114.BukkitVersionHelperSpigot114;
-import org.dynmap.bukkit.helper.v114_1.BukkitVersionHelperSpigot114_1;
+import org.dynmap.bukkit.helper.v115.BukkitVersionHelperSpigot115;
 
 public class Helper {
     
     public static final BukkitVersionHelper getHelper() {
         if (BukkitVersionHelper.helper == null) {
-                BukkitVersionHelper.helper = new BukkitVersionHelperSpigot114_1();
+		BukkitVersionHelper.helper = new BukkitVersionHelperSpigot115();
         }
         return BukkitVersionHelper.helper;
     }
